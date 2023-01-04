d6a768ddc2641576ace27d41cb8e4187b29696ba
Akhtool <Zelimhan1997@mail.ru>
2023-01-04 23:46:41 +0300

    feat: создать логику добавления и удаление карточек

---
+++ .history/scripts/script_20230103135052.js
@@ -0,0 +1,115 @@
+let editButton = document.querySelector('.profile__edit-button');
+let popupEditProfile = document.querySelector('.popup_type_profile-edit');
+let popupClose = document.querySelector('.popup__close-button_type_profile');
+let formElement = document.querySelector('.popup__input-form_type_profile');
+let nameInput = formElement.querySelector('.popup__input_type_name');
+let jobInput = formElement.querySelector('.popup__input_type_description');
+let profileName = document.querySelector('.profile__name');
+let profileDescription = document.querySelector('.profile__description');
+
+function openPopup() {
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileDescription.textContent;
+    popupEditProfile.classList.add('popup_opened');
+}
+function closePopup() {
+    popupEditProfile.classList.remove('popup_opened');
+}
+
+function handleFormSubmit (evt) {
+    evt.preventDefault();
+    profileName.textContent = nameInput.value;
+    profileDescription.textContent = jobInput.value;
+    closePopup();
+}
+
+editButton.addEventListener('click', openPopup);
+popupClose.addEventListener('click', closePopup);
+formElement.addEventListener('submit', handleFormSubmit); 
+
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+  const addCardElement = (cardName, cardLink) => {
+    console.log('addCard >>>', cardName, cardLink);
+    // Взять содержание из шаблона
+    const cardTemplate = document.querySelector('#card-template').content;
+    // Взять тег куда будем вносить данные из шаблона
+    const cardsList = document.querySelector('.cards__list');
+    // Клонировать содержание шаблона
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    // Заполнить содержимым
+    cardElement.querySelector('.card__name').textContent = cardName;
+    cardElement.querySelector('.card__image').src = cardLink;  
+
+    cardElement.querySelector('.card__like').addEventListener('click', function (evt) {
+        evt.target.classList.toggle('card__like_active');
+      });
+
+    // Отобразить на странице
+    cardsList.prepend(cardElement);
+  };
+
+  const addCard = (array) => {
+    array.forEach((element) => {
+        addCardElement(element.name, element.link);
+    });
+  };
+
+  addCard(initialCards);
+//попап добавлениея фотографий
+  const popupAddCard = document.querySelector ('.popup_type_add-card');
+  const addCardButton = document.querySelector('.profile__add-button'); 
+  const cardSaveButton = document.querySelector('.popup__save-button');
+  const cardElementName = document.querySelector('.popup__input_type_card-name');
+  const cardElementLink = document.querySelector('.popup__input_type_card-link');
+  const addCardForm = document.querySelector('.popup__input-form_type_card');
+  const popupCloseButton = document.querySelector('.popup__close-button_type_add-card');
+  
+  function openAddCardPopup() {
+    popupAddCard.classList.add('popup_opened');
+
+  }
+  
+  function closeAddCardPopup() {
+    popupAddCard.classList.remove('popup_opened');
+  }
+  
+  function handleAddCardForm (evt) {
+    evt.preventDefault();
+    addCardElement(cardElementName.value, cardElementLink.value);
+    closeAddCardPopup();
+}
+  
+  popupCloseButton.addEventListener('click', closeAddCardPopup);
+  addCardForm.addEventListener('submit', handleAddCardForm);
+  addCardButton.addEventListener('click', openAddCardPopup);
+
+  // лайк для фотографий
+
+//   cardElement.querySelector('.card__like').addEventListener('click', function (evt) {
+//     evt.target.classList.toggle('card__like_active');
+//   });
